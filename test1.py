bbba616585c2db223412803047e7a37228274303
Cyrus Garg <cyrus1@ualberta.ca>
2024-11-10 01:10:17 -0700

    save the changes

--- test1.py
+++ test1.py
@@ -2,8 +2,8 @@ from databasefunctions import *
 
 if __name__ == '__main__':
     setup_db()
-    add_user(12, "user12", "study buddy", "available", "Chemistry 101")
-    results = get_users_by_course_and_status("Math 101", "available")
+    add_user("user12", "study buddy", "available", "Chemistry 101","location")
+    results = get_users_by_course_and_status("Math 101", "available","location")
     for user in results:
         print(user)
     
