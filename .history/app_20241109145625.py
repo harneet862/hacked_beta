d467dd6acb6a5b16e8443c00487b06ddef2337ec
nathan c <nathanc@MacBook-Air-302.local>
2024-11-09 16:05:54 -0800

    Initial commit

--- .history/app_20241109145625.py
+++ .history/app_20241109145625.py
@@ -1,13 +1,13 @@
 from flask import Flask , render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
-import uuid
+import platform
 
 app = Flask(__name__)
 
 @app.route('/') #home route 
 
 def index():
-    system_id = uuid.uuid1()
+    system_id = platform.node()
     return render_template('index.html',system_id=system_id) #flask knows to look for this
 
 if __name__ == '__main__':
